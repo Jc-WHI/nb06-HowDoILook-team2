dd98af9fd8e59ad1bea6287175eab9fe5e120a77
YM_KIM <k.youmi3263@gmail.com>
2025-11-06 18:25:07 +0900

    chore: 초기 세팅

---
+++ src/apis/comment/comment.controller.js
@@ -0,0 +1,3 @@
+import prisma from '@prisma/client';
+
+const prisma = new PrismaClient();
---
+++ src/apis/comment/comment.router.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import { creatReply } from './reply.controller.js';
+
+const router = express.Router();
