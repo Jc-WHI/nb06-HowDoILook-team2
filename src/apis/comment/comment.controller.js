9f30c3e54c4ba204eea129ff54e2bc56f1ac7139
YM_KIM <k.youmi3263@gmail.com>
2025-11-06 18:52:45 +0900

    🚀 feat(comment): POST 답변 기능 추가

--- src/apis/comment/comment.controller.js
+++ src/apis/comment/comment.controller.js
@@ -1,3 +1,49 @@
 import prisma from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+const creatReply = async (req, res) => {
+  try {
+    const { curatingId } = req.params;
+    const { content, password } = req.body;
+
+    //curatingData = 특정 curating(Id)이 등록된 스타일 게시글의 정보를 받아온다.
+    const curatingData = await prisma.curating.findUnique({
+      where: { id: parseInt(curatingId) },
+      include: { style: true },
+    });
+
+    //큐레이팅 데이터가 없거나, 포함된 스타일 게시글을 못받아 온다면, return 404
+    if (!curatingData || !curatingData.style) {
+      return res.status(404).send({ message: '요청하신 데이터를 찾을 수 없습니다 :(' });
+    }
+
+    //큐레이팅데이터에.받아온스타일정보중에.패스워드를 styleData에 적용
+    const stylePassword = curatingData.style.password;
+
+    // if 입력된 password === stylePassword(style에 등록된 password) 일치하면
+    if (password === stylePassword) {
+      const replyData = await prisma.comment.create({
+        data: {
+          content,
+          curating: { connect: { id: parseInt(curatingId) } },
+        },
+        select: {
+          id: true,
+          nickname: true,
+          content: true,
+          createdAt: true,
+        },
+      });
+      res.status(201).json({ message: '답글이 등록되었습니다.', data: replyData });
+    } else {
+      return res.status(403).json({ message: '스타일 비밀번호와 일치하지 않습니다.' });
+    }
+  } catch (err) {
+    return console.error('에러코드', err);
+  } finally {
+    console.log('====== 답글 등록 테스트 종료 ======');
+  }
+};
+
+export { creatReply };
--- src/apis/comment/comment.router.js
+++ src/apis/comment/comment.router.js
@@ -2,3 +2,8 @@ import express from 'express';
 import { creatReply } from './reply.controller.js';
 
 const router = express.Router();
+
+router
+  .route('/:curatingId/reply')
+  //post
+  .post(creatReply);
