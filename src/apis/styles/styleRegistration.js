1adf35cb697c51d4c83a0fc8f977e08d87055dca
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-11-06 16:26:33 +0900

    server.js 및 스타일 등록 api 추가

---
+++ src/apis/styles/styleRegistration.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+class Style{
+    constructor(obj){
+        nickname,
+            title,
+            content,
+            password,
+            tags,
+            catecories,
+            imageUrls
+    }
+    styleregist(){
+        const  url = process.env.DATABASE_URL
+
+    }
+
+}
