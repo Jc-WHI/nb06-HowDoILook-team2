6967939cd88feba476338cdbfc8c10d8bc2bdf90
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-11-07 14:45:58 +0900

    style update 추가
    -스타일 업데이트 추가
    -req body 다루는 법 참조하세요

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import styleGetList from './src/apis/styles/styleGetList.js';
+import styleUpdate from './src/apis/styles/styleUpdate.js';
+const router = express.Router();
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use('/styles', styleGetList);
+app.patch("/api/styles/:styleId",async (req,res) =>{
+  await styleUpdate(req,res);
+  
+  
+
+
+})
 
 app.listen(PORT, () => {
   console.log('Server Start');
