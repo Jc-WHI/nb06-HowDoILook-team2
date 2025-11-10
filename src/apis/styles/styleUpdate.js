183f5e7a7e5ff6f8546b1ed1bc300ecc0d3af93b
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-11-10 10:22:44 +0900

    스타일 수정 개발 및 테스트 진행

--- src/apis/styles/styleUpdate.js
+++ src/apis/styles/styleUpdate.js
@@ -1,4 +1,4 @@
-import prisma from "./prismaClient.js";
+import prisma from "../prismaClient.js";
 
 
 const styleUpdate =  async (req,res)=>{
---
+++ src/routers/styleRouter.js
@@ -0,0 +1,12 @@
+import {prisma} from '../apis/prismaClient';
+import {Router} from 'express';
+
+const router = Router();
+
+router.route('/style:styleId')
+.put((req,res,next)=>{
+
+}
+)
+
+
