31e2040b36a7d76b41bdd12fda762283157e07ba
오윤 <passfile2@naver.com>
2025-11-10 17:19:34 +0900

    🚀 feat : styleRouter.js에 styleGetId 연결

--- src/routers/styleRouter.js
+++ src/routers/styleRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { styleListGallery, styleListRank } from '../controllers/styleGetController.js';
+import { styleListGallery, styleListRank, styleGetId } from '../controllers/styleGetController.js';
 const styleRouter = express.Router();
 
 styleRouter.get('/styles', styleListGallery);
 styleRouter.get('/ranking', styleListRank);
+styleRouter.get('/style/:styleId', styleGetId);
+
 export default styleRouter;
