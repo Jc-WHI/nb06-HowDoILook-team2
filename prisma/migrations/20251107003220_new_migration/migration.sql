af2d5ccc0d53c65e36dd546f28b69306d2aef576
오윤 <passfile2@naver.com>
2025-11-10 15:33:06 +0900

    🔄 chore(update): 스키마수정, 디렉토리구성 및 초기세팅

--- prisma/migrations/20251107003220_new_migration/migration.sql
+++ prisma/migrations/20251107003220_new_migration/migration.sql
@@ -1,60 +1,61 @@
 -- CreateEnum
-CREATE TYPE "Category" AS ENUM ('Top', 'Bottom', 'Outer', 'Onepiece', 'Shoes', 'Bag', 'Accessory');
+CREATE TYPE "Category" AS ENUM ('top', 'bottom', 'outer', 'dress', 'shoes', 'bag', 'accessory');
 
 -- CreateTable
 CREATE TABLE "Style" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "nickname" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "viewCount" INTEGER NOT NULL DEFAULT 0,
+    "curationCount" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "viewCount" INTEGER NOT NULL,
-    "curatingCount" INTEGER NOT NULL,
 
     CONSTRAINT "Style_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Item" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "brandName" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "type" "Category" NOT NULL,
-    "styleId" INTEGER NOT NULL,
-
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Tag" (
-    "id" SERIAL NOT NULL,
-    "tag" TEXT NOT NULL,
-    "styleId" INTEGER,
-
-    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Image" (
     "id" SERIAL NOT NULL,
-    "url" TEXT NOT NULL,
+    "imageUrls" TEXT NOT NULL,
     "styleId" INTEGER NOT NULL,
 
     CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "tags" TEXT NOT NULL,
+    "styleId" INTEGER,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "categories" "Category" NOT NULL,
+    "styleId" INTEGER NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Curating" (
     "id" SERIAL NOT NULL,
-    "trendy" INTEGER NOT NULL,
-    "individuality" INTEGER NOT NULL,
-    "practicality" INTEGER NOT NULL,
-    "valueForMoney" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
     "nickname" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "trendy" INTEGER NOT NULL DEFAULT 0,
+    "personality" INTEGER NOT NULL DEFAULT 0,
+    "practicality" INTEGER NOT NULL DEFAULT 0,
+    "costEffectiveness" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "styleId" INTEGER,
 
     CONSTRAINT "Curating_pkey" PRIMARY KEY ("id")
@@ -64,6 +65,7 @@ CREATE TABLE "Curating" (
 CREATE TABLE "Comment" (
     "id" SERIAL NOT NULL,
     "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "curatingId" INTEGER,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
@@ -73,13 +75,13 @@ CREATE TABLE "Comment" (
 CREATE UNIQUE INDEX "Comment_curatingId_key" ON "Comment"("curatingId");
 
 -- AddForeignKey
-ALTER TABLE "Item" ADD CONSTRAINT "Item_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Image" ADD CONSTRAINT "Image_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Tag" ADD CONSTRAINT "Tag_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Image" ADD CONSTRAINT "Image_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Item" ADD CONSTRAINT "Item_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Curating" ADD CONSTRAINT "Curating_styleId_fkey" FOREIGN KEY ("styleId") REFERENCES "Style"("id") ON DELETE SET NULL ON UPDATE CASCADE;
