a12147ff831accaa3e6f80b2a25bf2ab36777ceb
YM_KIM <k.youmi3263@gmail.com>
2025-11-10 18:27:30 +0900

    🔄 chore: import commentRouter in server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './lib/constants.js';
+import commentRouter from './routers/comment.router.js';
 
 const app = express();
 
