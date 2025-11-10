ecae5eeb902d411ecf53198f28673246f99abd62
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-11-10 17:00:18 +0900

    라우터 추가

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './lib/constants.js';
+import styleRouter from './routers/StyleRouter.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//test
+
+app.use(styleRouter);
 
 app.listen(PORT, () => {
   console.log('Server Start');
