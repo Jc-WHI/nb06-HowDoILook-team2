0f167acb92b7b0d8242513974dc8d27fbc2ed89e
YM_KIM <k.youmi3263@gmail.com>
2025-11-14 13:55:37 +0900

    🔧 fix(prisma): 스키마 재구성

--- prisma/migrations/20251114045339_/migration.sql
+++ prisma/migrations/20251114045339_/migration.sql
@@ -55,7 +55,7 @@ CREATE TABLE "Curating" (
     "practicality" INTEGER NOT NULL DEFAULT 0,
     "costEffectiveness" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "styleId" INTEGER,
+    "styleId" INTEGER NOT NULL,
 
     CONSTRAINT "Curating_pkey" PRIMARY KEY ("id")
 );
@@ -65,7 +65,7 @@ CREATE TABLE "Comment" (
     "id" SERIAL NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "curatingId" INTEGER,
+    "curatingId" INTEGER NOT NULL,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
@@ -75,6 +75,9 @@ CREATE TABLE "_StyleTags" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_tags_key" ON "Tag"("tags");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Comment_curatingId_key" ON "Comment"("curatingId");
