97bbe8c13e064653a633bed5e7613e9715aaea9a
YM_KIM <k.youmi3263@gmail.com>
2025-11-14 17:15:41 +0900

    🔧 fix(comment): 잘못된 app.use 경로를 올바르게 수정

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+<<<<<<< HEAD
+=======
+app.use('/curations', commentRouter);
+
+>>>>>>> 35f7bc7 (🔧 fix(comment): 잘못된 app.use 경로를 올바르게 수정)
 app.listen(PORT, () => {
   console.log('Server Start');
 });
