76f46909bb07c4a8156c423ea4933d82bc77e9d2
오윤 <passfile2@naver.com>
2025-11-17 12:06:50 +0900

    🗑️ remove : 상세 조회 브런치를 만들기 위한 정리

--- src/routers/styleRouter.js
+++ src/routers/styleRouter.js
@@ -3,7 +3,4 @@ import { styleListGallery, styleListRank } from '../controllers/styleGetControll
 import { withAsync } from '../lib/withAsync.js';
 const styleRouter = express.Router();
 
-styleRouter.get('/styles', withAsync(styleListGallery));
-styleRouter.get('/ranking', withAsync(styleListRank));
-
 export default styleRouter;
