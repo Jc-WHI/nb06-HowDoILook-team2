f4d97eee8284b06b10044b31f71994af4ad0dafe
오윤 <passfile2@naver.com>
2025-11-17 12:09:36 +0900

    🚀 feat : 스타일 상세 조회 스트럭츠 추가

--- src/structs/styleStruct.js
+++ src/structs/styleStruct.js
@@ -1,1 +1,12 @@
 import * as s from 'superstruct';
+
+// style id 검사
+const integer = s.refine(
+  s.coerce(s.number(), s.string(), (v) => Number(v)),
+  'positiveInteger',
+  (v) => Number.isInteger(v) && v >= 0,
+);
+
+export const styleIdStruct = s.object({
+  styleId: integer,
+});
