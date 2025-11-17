daeb866eca2fed9c534e30e3b60232342a518298
오윤 <passfile2@naver.com>
2025-11-17 12:12:42 +0900

    🔄 chore(update) : styleGetController.js에 import 수정

--- src/routers/styleRouter.js
+++ src/routers/styleRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { styleListGallery, styleListRank } from '../controllers/styleGetController.js';
+import { styleGetId } from '../controllers/styleGetController.js';
 import { withAsync } from '../lib/withAsync.js';
 const styleRouter = express.Router();
+
+styleRouter.get('/styles/:styleId', withAsync(styleGetId));
 
 export default styleRouter;
